e792e3ea03f132591f6870c22c7f7599c9dce4b0
Sebastian Döll <sebastian@katallaxie.dev>
2025-04-10 14:08:27 +0000

    docs: add simple example to stdout

---
+++ examples/io/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	htmx "github.com/katallaxie/fiber-htmx"
+)
+
+func Demo() htmx.Node {
+	return htmx.HTML5(
+		htmx.HTML5Props{
+			Title:    "error",
+			Language: "en",
+			Head:     []htmx.Node{},
+		},
+		htmx.Div(
+			htmx.Text("Hello world!"),
+		),
+	)
+}
+
+func main() {
+	Demo().Render(os.Stdout)
+}
