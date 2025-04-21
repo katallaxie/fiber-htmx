8b375f3028f4481d67c1797b6d52fd2d22065370
Sebastian Döll <sebastian@katallaxie.dev>
2025-04-21 19:10:35 +0000

    feat: add stack directions

--- components/stacks/stacks.go
+++ components/stacks/stacks.go
@@ -4,8 +4,7 @@ import htmx "github.com/katallaxie/fiber-htmx"
 
 // StackProps is a struct that contains the properties of a stack.
 type StackProps struct {
-	// ClassNames is a map of class names.
-	ClassNames htmx.ClassNames
+	htmx.ClassNames
 }
 
 // Stack is a function that returns a stack.
@@ -14,6 +13,48 @@ func Stack(props StackProps, children ...htmx.Node) htmx.Node {
 		htmx.Merge(
 			htmx.ClassNames{
 				"stack": true,
+			},
+			props.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// StackTop is a function that returns a stack with the top class.
+func StackTop(props StackProps, children ...htmx.Node) htmx.Node {
+	return htmx.Div(
+		htmx.Merge(
+			htmx.ClassNames{
+				"stack":     true,
+				"stack-top": true,
+			},
+			props.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// StackStart is a function that returns a stack with the start class.
+func StackStart(props StackProps, children ...htmx.Node) htmx.Node {
+	return htmx.Div(
+		htmx.Merge(
+			htmx.ClassNames{
+				"stack":       true,
+				"stack-start": true,
+			},
+			props.ClassNames,
+		),
+		htmx.Group(children...),
+	)
+}
+
+// StackEnd is a function that returns a stack with the end class.
+func StackEnd(props StackProps, children ...htmx.Node) htmx.Node {
+	return htmx.Div(
+		htmx.Merge(
+			htmx.ClassNames{
+				"stack":     true,
+				"stack-end": true,
 			},
 			props.ClassNames,
 		),
